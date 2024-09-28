4a4008399c30c1b6039700f74d8d484b5eb24ddd
edrissbenafia <edriss.benafia@isimm.u-monastir.tn>
2024-09-28 11:35:00 +0100

    first commit

---
+++ src/main/java/sesame/jenkins_pipeline/JenkinsPipelineApplication.java
@@ -0,0 +1,14 @@
+package sesame.jenkins_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JenkinsPipelineApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(JenkinsPipelineApplication.class, args);
+    }
+
+}
